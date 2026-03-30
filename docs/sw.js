a11f4a0d8bcd352f19f97b3488e12a75f08af9f8
Dag Hovland <hovlanddag@gmail.com>
2026-03-30 23:09:59 +0200

    debug history viewer

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daglifts-v3';
+const CACHE = 'daglifts-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
