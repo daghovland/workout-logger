29c40a92289968c6652b1ac571241a39860ef25b
Dag Hovland <hovlanddag@gmail.com>
2026-04-02 18:11:34 +0200

    mobile phone caching issue

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daglifts-v8';
+const CACHE = 'daglifts-v9';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
