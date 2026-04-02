abe5101e4398d7a0c263c6f1dd40ab393f6b3d40
Dag Hovland <hovlanddag@gmail.com>
2026-04-02 19:19:40 +0200

    log entry editing

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daglifts-v13';
+const CACHE = 'daglifts-v14';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
